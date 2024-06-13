dd3e63a3c8356e501b929e9a564f1d1f5f9c2898
TatiLisitskaya <tati.lisicka@gmail.com>
2024-06-13 16:38:29 +0300

    third commit

--- src/test/java/FirstTestWithParameters.java
+++ src/test/java/FirstTestWithParameters.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,18 +10,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 @DisplayName("Проверка количества результатов в поисковом запросе")
 public class FirstTestWithParameters {
+    SelenideElement input = $("[data-test-id=input__search]"),
+            button = $("[data-test-id=button__search]");
+    ElementsCollection listProducts = $$("[data-test-id=list__products]>div");
+    String baseUrl = "https://kazanexpress.ru/";
+    int quantity = 10;
+
     @BeforeEach
     void setup() {
-        open("https://kazanexpress.ru/");
+        open(baseUrl);
     }
-        @ValueSource(strings = {
-                "Корм для кошек", "Корм для собак","Пилка для ногтей"
-        })
-        @ParameterizedTest(name = "В поисковой выдаче на kazanexpress должно отоброжаться 10 результатов по запросу {0}")
-        void simpleWithParameters (String testData){
-            $("[data-test-id=input__search]").setValue(testData);
-            $("[data-test-id=button__search]").click();
-            $$("[data-test-id=list__products]>div")
-                    .shouldHave(sizeGreaterThanOrEqual(10));
+    @ValueSource(strings = {
+            "Корм для кошек", "Корм для собак", "Пилка для ногтей"
+    })
+    @ParameterizedTest(name = "В поисковой выдаче на kazanexpress должно отоброжаться 10 результатов по запросу {0}")
+    void simpleWithParameters(String testData) {
+        input.setValue(testData);
+        button.click();
+        listProducts.shouldHave(sizeGreaterThanOrEqual(quantity));
     }
 }
